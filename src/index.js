3dc4bd9c0cc08ebfb83dddfb1afd3619db198ca0
Scott Arden-Jones <scott.arden-jones@spareroom.co.uk>
2018-11-30 14:54:07 +0000

    Unregister Service Worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 import App from './App.jsx';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 import 'semantic-ui-css/semantic.css';
 import 'basscss/css/basscss-important.css';
@@ -10,4 +10,5 @@ import './index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+unregister();
